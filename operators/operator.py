655ef4cd4d0319eee8a57f639486963a7f6d56b0
haixuanTao <hai-xuan.tao@student.ecp.fr>
2022-01-23 14:14:41 +0100

    first commit

---
+++ operators/operator.py
@@ -0,0 +1,39 @@
+##
+## Copyright (c) 2017, 2021 ADLINK Technology Inc.
+##
+## This program and the accompanying materials are made available under the
+## terms of the Eclipse Public License 2.0 which is available at
+## http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+## which is available at https://www.apache.org/licenses/LICENSE-2.0.
+##
+## SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+##
+## Contributors:
+##   ADLINK zenoh team, <zenoh@adlink-labs.tech>
+##
+
+from zenoh_flow import Inputs, Outputs, Source
+import time
+import numpy as np
+
+
+class MyState:
+    def __init__(self, configuration):
+        self.value = np.zeros((2, 2))
+
+
+class MySrc(Source):
+    def initialize(self, configuration):
+        return MyState(configuration)
+
+    def finalize(self, state):
+        return None
+
+    def run(self, _ctx, state):
+        state.value += 1
+        time.sleep(1)
+        return state.value.tobytes()
+
+
+def register():
+    return MySrc
