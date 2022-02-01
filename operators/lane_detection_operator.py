ffe30d88c31b991c1b18ae2ae6524a898e6fe61e
haixuanTao <hai-xuan.tao@student.ecp.fr>
2022-02-01 19:11:19 +0100

    lane net is not fully implemented

--- operators/lane_detection_operator.py
+++ operators/lane_detection_operator.py
@@ -1,16 +1,6 @@
 ##
-## Copyright (c) 2017, 2021 ADLINK Technology Inc.
-##
-## This program and the accompanying materials are made available under the
-## terms of the Eclipse Public License 2.0 which is available at
-## http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
-## which is available at https://www.apache.org/licenses/LICENSE-2.0.
-##
-## SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
-##
-## Contributors:
-##   ADLINK zenoh team, <zenoh@adlink-labs.tech>
-##
+## The Lane detection operator is not yet fully implemented.
+
 
 from zenoh_flow import Inputs, Outputs, Operator
 import time
@@ -115,10 +105,3 @@ class MyOp(Operator):
 
 def register():
     return MyOp
-
-
-op = MyOp([])
-conf = op.initialize([])
-
-
-op.run([], conf, np.zeros((23, 23, 4), dtype=np.dtype("uint8")))
